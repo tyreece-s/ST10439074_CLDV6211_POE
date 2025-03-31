6f805ea7cae2379d7cbc8d9f21e91008ffc6b206
tyreece-s <st10439074@vcconnect.edu.za>
2025-03-31 18:21:57 +0200

    Modified the Booking Index|Delete|Details Razor Views for date value for Readability. p.s prev commit was for Event Razor Views date & time value

--- EventEase/obj/Debug/net9.0/EventEase.AssemblyInfo.cs
+++ EventEase/obj/Debug/net9.0/EventEase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bc8003065b498751061f490f8c10b3c5bb2d06b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6019cee6edd9e03a21866af5b9504bb676ab2bf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
